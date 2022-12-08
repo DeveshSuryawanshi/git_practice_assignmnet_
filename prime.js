30e21ce14fc2cea79d7a5db02a7467962fe09e03
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2022-12-08 21:25:48 +0530

    Code is Corrected now

--- prime.js
+++ prime.js
@@ -1,6 +1,9 @@
 function checkPrime(num){
-    for(let i=0; i<num; i++){
-        console.log(num);
+    let count = 0;
+    for(let i=1; i<=num; i++){
+        if(num %i == 0){
+            count++;
+        }
     }
 
     if(count == 2){
