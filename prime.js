90469590ca0505a4a45dae6d89949d0841261e75
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2022-12-08 21:05:02 +0530

    new files were pushed

---
+++ prime.js
@@ -0,0 +1,14 @@
+function checkPrime(num){
+    for(let i=0; i<num; i++){
+        console.log(num);
+    }
+
+    if(count == 2){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }    
+}
+
+let num = 7; 
+checkPrime(num);
